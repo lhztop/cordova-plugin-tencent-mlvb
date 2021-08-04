2bdc3ab9884dad49d60869c6325b3ec82a56d9fd
lhz <lhztop@163.com>
2021-08-04 15:18:14 +0800

    add prefence

--- src/android/hooks/before_plugin_install/unzip_libs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd $(dirname $0)
-cd ../..
-
-if [ ! -e libs ]; then unzip libs.zip; fi
-if [ -e libs.zip ]; then rm libs.zip; fi
--- src/ios/hooks/before_plugin_install/unzip_libs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd $(dirname $0)
-cd ../..
-
-if [ ! -e TXRTMPSDK.framework ]; then unzip TXRTMPSDK.framework.zip; fi
-if [ -e TXRTMPSDK.framework.zip ]; then rm TXRTMPSDK.framework.zip; fi
